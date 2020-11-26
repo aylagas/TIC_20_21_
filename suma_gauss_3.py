a35355dcd409c27b198fe0ce4df4c932f87e670d
aylagas <71437815+aylagas@users.noreply.github.com>
2020-11-26 12:31:19 +0100

    Delete suma_gauss_3.py

--- suma_gauss_3.py
+++
@@ -1,9 +0,0 @@
-def suma_gauss_3():
-    n_final=input("introduce hasta que numero ")
-    suma_acumulada=0
-    for cont in range(1,n_final+1):
-        if(cont%2==0):
-            suma_acumulada= suma_acumulada+cont
-    print "suma_acumulada= ",suma_acumulada
-
-suma_gauss_3()
