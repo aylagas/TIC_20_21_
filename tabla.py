dbf72c28bbd4b253b4010141533086ba15fd83ac
aylagas <71437815+aylagas@users.noreply.github.com>
2020-11-26 12:32:16 +0100

    Delete tabla.py

--- tabla.py
+++
@@ -1,8 +0,0 @@
-def tabla():
-   n_tabla=input("Dime la tabla que deseas escribir " )
-   print "TABLA DEL ", n_tabla
-   for cont in range(0, 10):
-       print n_tabla," x ",cont ," = ", n_tabla*cont
-
-tabla()
-   
