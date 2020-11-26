12c0f6ed26107aa89008c9695047cb598d70d15d
aylagas <71437815+aylagas@users.noreply.github.com>
2020-11-26 12:37:34 +0100

    Delete contrasena.py

--- contrasena.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: cp1252 -*-
-def contrasena():
-    nombre=raw_input("Intrduce el nombre: ")
-    apellido=raw_input("Introduce el apellido: ")
-    print nombre," ",apellido, "WAS an impostor"
-    print "Tu lindo nombre empieza por la ",3*nombre+2*apellido
-contrasena()
