20782e765199b4049564b699af1f9741790eadbf
aylagas <71437815+aylagas@users.noreply.github.com>
2021-02-22 17:24:42 +0100

    Create apuntes.cpp

---
+++ C++/apuntes.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+	//Definimos las variables
+	int numeros[10];
+	char palabra[5];
+	float notas[3];
+	for(cont=0;cont<=10;cont++){
+	}
+	while(x<10){
+	}
+	if(a<b){
+	}else{
+	}
+	do{
+	}while(a<3);	
+}
