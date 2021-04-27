8f121c0d451a5648cecbabc2889d60c712cb61a0
aylagas <71437815+aylagas@users.noreply.github.com>
2021-04-27 20:55:00 +0200

    Update HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Ricardo el Calvo");
+		System.out.print("Prueba");
 	}
 
 }
